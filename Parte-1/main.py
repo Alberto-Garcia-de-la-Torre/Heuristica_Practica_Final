48c86ec7e529ee6436a6f7f32f8002add2ea7448
100472039 <95372999+100472039@users.noreply.github.com>
2024-12-16 09:24:04 +0100

    restriccion 1 terminada

--- Parte-1/main.py
+++ Parte-1/main.py
@@ -1,7 +1,52 @@
 from constraint import *
 import sys
 import csv
-    
+
+class CSP:
+    def __init__(self, franjas, dimensiones, STD, SPC, PRK, aviones):
+        self.franjas = franjas
+        self.dimensiones = dimensiones
+        self.STD = STD
+        self.SPC = SPC
+        self.PRK = PRK
+        self.aviones = aviones
+        self.problema = Problem()
+
+    def addvariables(self):
+        self.variables = []
+        for f in range(self.franjas):
+            for avion in self.aviones:
+                variable = (avion, f)
+                self.problema.addVariable(variable, self.PRK+self.STD+self.SPC)
+                self.variables.append(variable)
+
+    def no_dos_JMB_en_taller(*res):
+        # Res es el valor de restriccion, que es una lista con tres valores: puntero, valor de variable1, valor de variable2
+        if res[1][1] != "PRK" and res[2][1] != "PRK":
+            if res[1] == res[2]:
+                return False
+        return True
+
+    def restricciones(self):
+        variables_por_franja = []
+        for franja in range(self.franjas):
+            variables_por_franja.append([])
+        for variable in self.variables:
+            variables_por_franja[variable[1]].append(variable)
+        # No puede haber más de dos aviones en un taller
+        for franja in variables_por_franja:
+            aviones_JMB = []
+            for variable in franja:
+                if variable[0][1] == "JMB":
+                    aviones_JMB.append(variable)
+            self.problema.addConstraint(self.no_dos_JMB_en_taller, aviones_JMB)
+
+    def realizar_problema(self):
+        self.addvariables()
+        self.restricciones()
+        soluciones = self.problema.getSolution()
+        print(soluciones)
+
 def leer_archivo(path):
     archivo = open(path, 'r')
     filas = []
@@ -27,79 +72,11 @@ def leer_archivo(path):
         aviones.append((int(fila[0]), str(fila[2:5]), str(fila[6]), int(fila[8]), int(fila[10])))
     return franjas, dimensiones, STD, SPC, PRK, aviones
 
-def addvariables(aviones, STD, SPC, PRK, franjas):
-    problema = Problem()
-    variables = []
-    for f in range(franjas):
-        for avion in aviones:
-            problema.addVariable((avion, f), PRK+STD+SPC)
-            variables.append((avion, f))
-    return problema, variables
-
-def restricciones(problema: Constraint, variables, franjas):
-    # No puede haber más de dos aviones en un taller
-    aviones_franja = []
-    for i in range(franjas):
-        aviones_franja.append([])
-    for variable in variables:
-        aviones_franja[variable[1]].append(variable)
-    for franja in aviones_franja:
-        for avion in franja:
-
-
-
-
-
-
-
-
-
-
-    for f in range(franjas):
-        # Para cada tipo de taller (STD, SPC, PRK)
-        for taller in STD + SPC + PRK:
-            # Crear una lista de variables que representan aviones en esa franja y taller
-            aviones_en_taller = []
-            for variable in variables:
-                avion, franja = variable
-                if franja == f and taller in problema.getDomain(variable):  # Verificar si el taller está en el dominio
-                    aviones_en_taller.append(variable)
-
-            # Añadir la restricción de que no pueden haber más de 2 aviones en el mismo taller en la misma franja
-            if len(aviones_en_taller) > 2:
-                # Por ejemplo, puedes agregar una restricción que garantice que no se asignen más de 2 aviones en el taller
-                problema.addConstraint(lambda *asignaciones: len(set(asignaciones)) <= 2, aviones_en_taller)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    # Evitar que dos Jumbos estén en el mismo taller
-    for variable in variables:
-        for otra_variable in variables:
-            if variable != otra_variable and variable[1] == otra_variable[1] and variable[0][1] == "JMB" and otra_variable[0][1] == "JMB":
-                problema.addConstraint(lambda a,b: a!=b, (variable, otra_variable))
-
-def realizar_problema(path):
-    franjas, dimensiones, STD, SPC, PRK, aviones = leer_archivo(path)
-    problema, variables = addvariables(aviones, STD, SPC, PRK, franjas)
-    restricciones(problema, variables, franjas)
-    soluciones = problema.getSolution()
-    print(soluciones)
-
-
 def main():
-    realizar_problema(sys.argv[1])
+    franjas, dimensiones, STD, SPC, PRK, aviones = leer_archivo(sys.argv[1])
+    print(STD+SPC+PRK)
+    csp = CSP(franjas, dimensiones, STD, SPC, PRK, aviones)
+    csp.realizar_problema()
 
 if __name__ == "__main__":
     main()
