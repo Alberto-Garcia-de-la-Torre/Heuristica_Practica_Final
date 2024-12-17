a9f367fe9a392ae60f2c5f3d5721f3ebe2e3d7b8
100472039 <95372999+100472039@users.noreply.github.com>
2024-12-17 10:54:21 +0100

    Lectura de archivo

---
+++ Parte-2/main.py
@@ -0,0 +1,24 @@
+import sys
+
+
+def leer_archivo(path):
+    archivo = open(path, 'r')
+    filas = []
+    for fila in archivo:
+        filas.append(fila)
+    n_aviones = int(filas[0][0])
+    aviones = []
+    for avion in range(n_aviones):
+        aviones.append([[int(filas[avion+1][1]), int(filas[avion+1][3])], [int(filas[avion+1][7]), int(filas[avion+1][9])]])
+    matriz = []
+    for linea in range(len(filas)-len(aviones)-1):
+        matriz.append(filas[linea+len(aviones)+1].split(";"))
+        matriz[-1][-1] = matriz[-1][-1][0] # Quitar \n
+    return aviones, matriz
+def main():
+    aviones, matriz = leer_archivo(sys.argv[1])
+    # soluciones = csp.realizar_problema()
+    # escribir_archivo(sys.argv[2], soluciones)
+
+if __name__ == "__main__":
+    main()
